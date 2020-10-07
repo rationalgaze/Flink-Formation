206da6ca9633146ebe190a93a7cd432ca0e71a26
Nikolai Vorotnikov <nikolai.vorotnikov@altran.com>
2020-10-07 10:17:47 +0200

    init Graph example

---
+++ gelly/src/main/scala/Graph.scala
@@ -0,0 +1,8 @@
+
+object Graph {
+  def main(args: Array[String]): Unit = {
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    val vertices:DataSet[Vertex[String, Long]] = _
+    val v = new Vertex(1L, "foo")
+  }
+}
